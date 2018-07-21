5f30969e3d8dfc6247fa2c421b35a370eb7388b0
Hider Wang <724598894@qq.com>
2018-07-21 23:40:49 +0800

    前端静态文件

---
+++ resources/views/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <link rel="stylesheet" href="/css/weui.css">
+    <link rel="stylesheet" href="book.css">
+    <title>@yield('title')</title>
+</head>
+<body>
+
+@yield('content')
+</body>
+<script src="/js/jquery-1.11.2.min.js"></script>
+@yield('my-js')
+</html>
