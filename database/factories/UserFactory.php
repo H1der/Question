3afa8696ea04e4e99f3960f20ece51b4ba4862bd
Hider Wang <724598894@qq.com>
2018-09-23 16:47:35 +0800

    话题选择

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,12 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(App\Topic::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'bio' => $faker->paragraph,
+        'questions_count' => 1,
+    ];
+});
