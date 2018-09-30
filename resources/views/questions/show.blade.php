4eeb4aca7d38b52bce4ec4ee0cb0dad5c496c7bc
Hider Wang <724598894@qq.com>
2018-09-30 22:47:34 +0800

    话题编辑和更新完成

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -7,12 +7,18 @@
                 <div class="card">
                     <div class="card-header">{{$question->title}}
                     @foreach($question->topics as $topic)
-                        <a class="topic">{{ $topic->name }}</a>
+                        <a class="topic" href="/topic/{{ $topic->id }}">{{ $topic->name }}</a>
                         @endforeach
                     </div>
 
                     <div class="card-body">
                         {!! $question->body !!}
+                    </div>
+
+                    <div class="actions">
+                        @if(\Illuminate\Support\Facades\Auth::check()&&\Illuminate\Support\Facades\Auth::user()->owns($question))
+                            <span class="edit"><a href="/questions/{{ $question->id }}/edit">编辑</a></span>
+                        @endif
                     </div>
                 </div>
             </div>
